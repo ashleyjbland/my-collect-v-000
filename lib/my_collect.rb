ee44f92cc7a0572a65c46a368884eb122d3bb049
Ashley Bland <ashleyjbland@gmail.com>
2018-09-03 12:57:08 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(collection)
+  counter = 0
+  upcase_collection = []
+  while collection.length < counter
+    upcase_collection << yield(collection, lang)
+    counter += 1
+  end
+  upcase_collection
+end
+    
+my_collect(collection) do |lang|
+  lang.upcase
+end
